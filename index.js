eb6a838ea37d4a8e65834cb77a70eba8c5d3f391
sabnira <sabnirajeemi@gmail.com>
2026-04-16 18:13:49 +0600

    jwt verify token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -34,9 +35,55 @@ async function run() {
 
 
 
-    //users related api
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      // create token
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
 
-    app.get('/users', async (req, res) => {
+      res.send({ token })
+
+      // console.log(token);
+      // res.cookie('token', token, {
+      //   httpOnly: true,
+      //   secure: process.env.NODE_ENV === 'production',
+      //   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      // })
+      //   .send({ success: true })
+    })
+
+
+    // verifyToken middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
+
+    }
+
+
+
+    //users related api
+    app.get('/users', verifyToken, async (req, res) => {
+      console.log(req.headers);
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result)
     })
@@ -45,10 +92,10 @@ async function run() {
       const user = req.body;
       //insert email if user doesnt exists
       // can do this many ways (1. email unique, 2. upsert, 3. simple checking)
-      const query = { email: user.email}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
-      if(existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
       res.send(result)
